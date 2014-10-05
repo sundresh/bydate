ef5baaadb3df07176d9461e7cdb22cdb336ed0ae
Sameer Sundresh <sameer@sundresh.org>
2014-10-05 03:34:22 -0700

    Added script

---
+++ bydate.sh
@@ -0,0 +1,18 @@
+export BYDATE_BASE=~/bydate
+
+function td () {
+	cd $BYDATE_BASE
+
+	local today=`date +%Y/%m/%d`
+	ln -nsf $today today
+	mkdir -p $today && cd $today
+
+	export TODAY=`pwd`
+}
+
+function td () {
+	cd $BYDATE_BASE
+
+	local yesterday=`date +%Y/%m/%d -d yesterday`
+	mkdir -p $yesterday && cd $yesterday
+}
